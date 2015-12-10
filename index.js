e1140649478ed5ce2d5fcc37a9fee53b57a8755d
pernas <jaume@pernas.cat>
2015-12-09 19:09:32 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var request = require('request');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+});
+
+app.post('/feedback', function (req, res) {
+  var jira    = 'https://blockchain.atlassian.net/rest/collectors/1.0/template/feedback/e6ce4d72'
+    , headers = { 'X-Atlassian-Token': 'nocheck' };
+  request.post({
+    url: jira,
+    headers: headers,
+    form: req.body
+  }, function (err, httpResponse, body) {
+    res.json({ success: !(err != null) });
+  });
+});
